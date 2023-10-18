a7f9f87c6a29cd2a17fc0c57ae8eb68856b9dea2
passionde <yarikdev@bk.ru>
2023-10-18 20:46:55 +0500

    <add>: Completed practical task 7

--- blog/app/models.py
+++ blog/app/models.py
@@ -1,7 +1,34 @@
 """
 Definition of models.
 """
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+from django.contrib import admin
+from django.urls import reverse
+
+
+# Модель данных блога
+class Blog(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=100, unique_for_date="posted", verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Краткое содержание")
+    content = models.TextField(verbose_name="Полное содержание")
+    posted = models.DateTimeField(default=datetime.now(), db_index=True, verbose_name="Опубликована")
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="Автор")
+
+    def get_absolute_url(self):
+        return reverse("blogpost", args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "Posts"
+        ordering = ["posted"]
+        verbose_name = "статья блога"
+        verbose_name_plural = "статьи блога"
+
+
+admin.site.register(Blog)
